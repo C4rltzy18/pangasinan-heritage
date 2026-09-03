8a0b82a01f53a07317cf4c24fed7ee841264cc9f
C4rltzy18 <carlvincenttanguilig12@gmail.com>
2026-09-03 10:54:25 +0800

    Add basePath and assetPrefix for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 
+const isGithubPages = process.env.GITHUB_ACTIONS || process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+
+  basePath: isGithubPages ? '/pangasinan-heritage' : '',
+  assetPrefix: isGithubPages ? '/pangasinan-heritage/' : '',
 
   images: {
     unoptimized: true,
