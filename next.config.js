909c0d7a08f16749ca74b27351cadc630cea64d0
Carl Vincent Tanguilig <carlvincenttanguilig12@gmail.com>
2026-09-03 11:36:18 +0800

    Fix Next.js config for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-<<<<<<< HEAD
-const isGithubPages = process.env.GITHUB_ACTIONS || process.env.NODE_ENV === 'production';
-=======
 const isGithubPages =
   process.env.GITHUB_ACTIONS || process.env.NODE_ENV === 'production';
->>>>>>> 8186467 (Fix GitHub Pages CSS paths)
 
 const nextConfig = {
   output: 'export',
